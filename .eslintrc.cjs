3272c25ed7afca6f68b19c039084476bcfc8e497
Ahmed Hassan <ahmedmotawi8@gmail.com>
2024-06-05 02:23:26 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,76 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'react-app',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
+
+// module.exports = {
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:react/jsx-runtime",
+//     "plugin:react-hooks/recommended",
+//     "react-app",
+//   ],
+//   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+//   settings: { react: { version: "18.2" } },
+//   plugins: ["react-refresh"],
+//   rules: {
+//     "react-refresh/only-export-components": [
+//       "warn",
+//       { allowConstantExport: true },
+//     ],
+//     "react/prop-types": "off",
+//   },
+// };
+
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     es2020: true,
+//     //remove the module unknown in the prritier config
+//     // node: true,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:react/jsx-runtime",
+//     "plugin:react-hooks/recommended",
+//     // this line will make the app not show if smth not in use or key problems
+//     // "react-app",
+//   ],
+//   ignorePatterns: ["dist", ".eslintrc.cjs"],
+//   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+//   settings: { react: { version: "18.2" } },
+//   plugins: ["react-refresh"],
+//   rules: {
+//     //var not in use will be yellow instead of red - warn instead error
+//     "no-unused-vars": "warn",
+//     //will not warn if i didn't pass props
+//     "react/prop-types": "off",
+//     "react-refresh/only-export-components": [
+//       "warn",
+//       { allowConstantExport: true },
+//     ],
+//   },
+// };
